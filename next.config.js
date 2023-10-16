da9de98a46b7f857752e87c3c1cc5641226b95dd
mtt2 <michaeltran20052006@yahoo.com>
2023-10-16 14:24:37 -0500

    convert to typescript and limit permissions

--- next.config.js
+++ next.config.js
@@ -1,17 +1,4 @@
-const isGithubActions = process.env.GITHUB_ACTIONS || false
+/** @type {import('next').NextConfig} */
+const nextConfig = {}
 
-let assetPrefix = ''
-let basePath = '/'
-
-if (isGithubActions) {
-  // trim off `<owner>/`
-  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
-
-  assetPrefix = `/${repo}/`
-  basePath = `/${repo}`
-}
-
-module.exports = {
-  assetPrefix: assetPrefix,
-  basePath: basePath,
-}
+module.exports = nextConfig
